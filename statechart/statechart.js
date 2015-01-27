bb88b1a9db04f5e7a38212eafe9b1d199b814441
chadeubanks <ceubanks@thecodeboutique.com>
2015-01-27 09:29:12 -0500

    Updates state chart class to support substates

--- statechart/statechart.js
+++ statechart/statechart.js
@@ -16,27 +16,37 @@
  */
 
 
-Alto.Statechart = Alto.Object.extend ({
+Alto.Statechart = Alto.Object.extend({
 
     /**
-        @property currentState
-    */
+     @property currentState
+     */
     currentState: "",
 
     /**
-        @method dispatchEvent
-    */
-    dispatchEvent: function(eventName) {
+     @property currentSubState
+     */
+    currentSubState: "",
+
+    /**
+     @method dispatchEvent
+     */
+    dispatchEvent: function (eventName) {
         var APP = Alto.applicationName,
-            state = window[APP].Statechart.currentState,
+            state = window[APP].statechart.currentState,
+            substate = window[APP].statechart.currentSubState,
             args = Array.prototype.slice.call(arguments),
             message = "Unknown method name: Your state is missing the method \"" + eventName + "\".";
 
         args.shift();
 
-        if (!window[APP][state]) {return}
+        if (!window[APP][state]) {
+            return
+        }
 
-        if (window[APP][state][eventName]) {
+        if (window[APP][substate] && window[APP][substate][eventName]) {
+            window[APP][substate][eventName].apply(this, args);
+        } else if (window[APP][state][eventName]) {
             window[APP][state][eventName].apply(this, args);
         } else {
             Alto.Console.log(message, Alto.Console.errorColor);
@@ -45,20 +55,20 @@ Alto.Statechart = Alto.Object.extend ({
     },
 
     /**
-        @method goToState
-    */
+     @method goToState
+     */
     goToState: function (state) {
         var APP = Alto.applicationName;
 
         // If we are already in a state, call is exitState before transitioning
-        if (window[APP].Statechart.get("currentState") != "") {
+        if (window[APP].statechart.get("currentState") != "") {
 
             if (window[APP].LogStateTransitions) {
-                var message = "Exiting " + window[APP].Statechart.get("currentState");
+                var message = "Exiting " + window[APP].statechart.get("currentState");
                 Alto.Console.log(message, Alto.Console.warnColor);
             }
 
-            window[APP][window[APP].Statechart.get("currentState")].exitState();
+            window[APP][window[APP].statechart.get("currentState")].exitState();
         }
 
         // Handle an attempt to enter a non existent state
@@ -66,15 +76,57 @@ Alto.Statechart = Alto.Object.extend ({
             var message = "Can not find state " + state + ". Check your applications router and declared states.";
             Alto.Console.log(message, Alto.Console.errorColor);
         } else {
-            window[APP].Statechart.set("currentState", state);
+            window[APP].statechart.set("currentState", state);
 
             if (window[APP].LogStateTransitions) {
-                var message = "Entering " + window[APP].Statechart.get("currentState");
+                var message = "Entering " + window[APP].statechart.get("currentState");
                 Alto.Console.log(message, Alto.Console.messageColor);
             }
 
             window[APP][state].enterState();
         }
+    },
+
+    /**
+     @method goToSubState
+     */
+    goToSubState: function (substate) {
+        // check if the current state has the substate
+        // enter the substate
+        // set the statecharts current substate
+        var APP = Alto.applicationName,
+            currentState  = window[APP].statechart.get("currentState");
+
+        if (window[APP][currentState][substate]) {
+            window[APP][substate] =  window[APP][currentState][substate].create();
+
+            window[APP].statechart.set("currentSubState", substate);
+
+            if (window[APP].LogStateTransitions) {
+                var message = "Entering substate " + window[APP].statechart.get("currentSubState");
+                Alto.Console.log(message, Alto.Console.messageColor);
+            }
+
+            window[APP][substate].enterState();
+
+        } else {
+            Alto.Logger.error('Substate', substate, 'not found in parent state');
+        }
+
+    },
+
+    leaveCurrentSubState: function() {
+        var APP = Alto.applicationName,
+            substate = window[APP].statechart.get("currentSubState");
+
+        if (window[APP].LogStateTransitions) {
+            var message = "Exiting substate " + substate;
+            Alto.Console.log(message, Alto.Console.warnColor);
+        }
+
+        window[APP].statechart.set("currentSubState", null);
+
+        window[APP][substate].exitState();
     }
 
 });
