37cd495ae31cb0c760d935555c78b950ce41c2b0
Chad Eubanks <ceubanks@thecodeboutique.com>
2015-09-25 12:52:49 -0400

    Extend key responder mixin to capture return key

--- ui/desktop/mixins/responder/key_responder.js
+++ ui/desktop/mixins/responder/key_responder.js
@@ -6,9 +6,12 @@ Alto.KeyResponderMixin = Alto.Mixin.create({
         if (!this.get('keyResponder')) {return};
 
         if (event.keyCode == 27) {
+            if (Alto.isNone(this.keyResponder.get('cancelAction'))) {return}
             window[Alto.applicationName].statechart.dispatchViewEvent(this.keyResponder.get('cancelAction'));
+        } else if (event.keyCode == 13) {
+            if (Alto.isNone(this.keyResponder.get('saveAction'))) {return}
+            window[Alto.applicationName].statechart.dispatchViewEvent(this.keyResponder.get('saveAction'));
         }
-
     }
 
 });
