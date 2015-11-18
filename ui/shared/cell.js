ea9309561cc4e93c955b2d1dce740874d4fa5823
Anthony Alviz <anthonyalviz@gmail.com>
2015-11-18 17:24:04 -0500

    - Change KVO lookup, getter was throwing an undefined error

--- ui/shared/cell.js
+++ ui/shared/cell.js
@@ -38,7 +38,8 @@ Alto.Cell = Alto.CoreView.extend ({
             this.set('data', recordAtIndex);
         }
 
-        if (this.get('isSelected') || (this.data && this.data.get('isSelected'))) {
+        debugger;
+        if (this.get('isSelected') || (this.data && this.data.isSelected)) {
             node.style.backgroundColor = '#eaf4ff'
         } else {
             node.removeAttribute('style');
@@ -52,7 +53,7 @@ Alto.Cell = Alto.CoreView.extend ({
     },
 
     isSelectedDidChange: function () {
-        if (this.get('isSelected') || (this.data && this.data.get('isSelected'))) {
+        if (this.get('isSelected') || (this.data && this.data.isSelected)) {
             this.node.style.backgroundColor = '#eaf4ff'
         } else {
             this.node.removeAttribute('style');
